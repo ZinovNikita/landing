cb0384c23aa552d2a62a80898772fa8959f5f44b
Zinov Nikita <zinov.nikita2010@yandex.ru>
2023-09-02 01:35:17 +0500

    Updates

--- assets/Room-589bb88b.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,t}from"./index-125f7831.js";const s={};function a(r,e){return t(r.$route.params.room_hash)}const _=o(s,[["render",a]]);export{_ as default};
---
+++ assets/Room-f5d84f67.js
@@ -0,0 +1,1 @@
+import{_ as o,t}from"./index-256458db.js";const s={};function a(r,e){return t(r.$route.params.room_hash)}const _=o(s,[["render",a]]);export{_ as default};
