1433d29380dc3e44d3e981e6ce2a3547995c7e67
Zinov Nikita <zinov.nikita2010@yandex.ru>
2023-09-02 00:35:54 +0500

    router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
-  <a-layout>
-    <a-layout-content :style="{ height: '100vh', padding: '64px' }">
-      <xmltmp/>
-    </a-layout-content>
-  </a-layout>
+  <router-view/>
 </template>
 
 <script setup lang="ts">
-import xmltmp from './components/XMLTemplates.vue'
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <a-input-group compact>
+    <a-input addon-before="Комната" v-model:value="room_hash" style="width: 20%" />
+    <a-input addon-before="Пользователь" v-model:value="user_hash" style="width: 20%" />
+    <a-button type="primary" @click="connect(user_hash || '', room_hash || '')">Войти</a-button>
+    <a-button @click="connect(user_hash || '')">Создать новую комнату</a-button>
+  </a-input-group>
+</template>
+<script lang="ts" setup>
+import { ref,  } from 'vue'
+import { useRouter } from 'vue-router'
+const room_hash = ref(localStorage.getItem('room_hash'))
+const user_hash = ref(localStorage.getItem('user_hash'))
+const router = useRouter()
+
+const connect = (user_hash: string, room_hash: string = 'new' ) => {
+  if(room_hash.length === 0 )
+    return alert('Укажите комнату')
+  localStorage.setItem('room_hash', room_hash)
+  localStorage.setItem('user_hash', user_hash)
+  router.push({ path: `/rooms/${room_hash}` })
+}
+</script>
