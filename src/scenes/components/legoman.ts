466c69eb7164d62adcc995ae8320e98259035179
Zinov Nikita <zinov.nikita2010@yandex.ru>
2023-09-06 23:12:26 +0500

    Настройка персонажа

--- src/scenes/components/legoman.ts
+++ src/scenes/components/legoman.ts
@@ -1,58 +1,105 @@
-import { SceneLoader, Animation } from "@babylonjs/core"
-import type { Scene, AbstractMesh } from "@babylonjs/core"
+import { SceneLoader, Animation, Color3 } from "@babylonjs/core"
+import type { Scene, AbstractMesh, MultiMaterial, PBRMaterial } from "@babylonjs/core"
 
 type LegoMan = {
   model: AbstractMesh
   Idle: () => void
   Walk: () => void
+  setColors: (body: string, pants: string, skin: string) => void
 }
 
-const legoMan = (scene: Scene) => new Promise<LegoMan>((resolve,reject) => {
-  SceneLoader.ImportMesh('', '/landing/', "legoman.babylon", scene, (meshes: AbstractMesh[]) => {
-    if (meshes.length !== 6)
-      return reject(new Error('wrong model'))
-
-    const [body, HandL, LegL, Head, LegR, HandR] = meshes,
-      animRotateL = new Animation("animRotateL", "rotation.x", 60, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_CYCLE),
-      animRotateR = new Animation("animRotateR", "rotation.x", 60, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_CYCLE)
-
-    animRotateL.setKeys([ { frame: 0,  value: 0 }, { frame: 25,  value: 1 }, { frame: 50,  value: 0 }, { frame: 75,  value: -1 }, { frame: 100,  value: 0 } ])
-    animRotateR.setKeys([ { frame: 0,  value: 0 }, { frame: 25,  value: -1 }, { frame: 50,  value: 0 }, { frame: 75,  value: 1 }, { frame: 100,  value: 0 } ])
-
-    HandL.animations.push(animRotateL)
-    LegL.animations.push(animRotateL)
-    HandR.animations.push(animRotateR)
-    LegR.animations.push(animRotateR)
-
-    Head.setParent(body)
-    HandL.setParent(body)
-    HandR.setParent(body)
-    LegL.setParent(body)
-    LegR.setParent(body)
-    
-    const legoman: LegoMan = {
-      model: body,
-      Idle: () => {
-        scene.stopAnimation(LegL)
-        scene.stopAnimation(LegR)
-        scene.stopAnimation(HandR)
-        scene.stopAnimation(HandL)
-        LegL.rotation.set(0,0,0)
-        LegR.rotation.set(0,0,0)
-        HandR.rotation.set(0,0,0)
-        HandL.rotation.set(0,0,0)
-      },
-      Walk() {
-        scene.beginAnimation(LegL, 0, 100, true)
-        scene.beginAnimation(LegR, 0, 100, true)
-        scene.beginAnimation(HandR, 0, 100, true)
-        scene.beginAnimation(HandL, 0, 100, true)
-      }
+class LegoManClass implements LegoMan {
+  static instance: LegoManClass
+  static async Init (scene: Scene) {
+    if (!LegoManClass.instance) {
+      const { meshes } = await SceneLoader.ImportMeshAsync('', '/landing/', "legoman.babylon", scene)
+      LegoManClass.instance = new LegoManClass(scene, meshes)
     }
-    resolve(legoman)
-  })
-})
+    return LegoManClass.instance
+  }
+
+  private _scene: Scene
+  private _Body: AbstractMesh
+  private _HandL: AbstractMesh
+  private _LegL: AbstractMesh
+  private _Head: AbstractMesh
+  private _LegR: AbstractMesh
+  private _HandR: AbstractMesh
+  
+  constructor (scene: Scene, meshes: AbstractMesh[]) {
+    this._scene = scene
+    if (meshes.length !== 6)
+      throw new Error('wrong model')
+
+    this._Body = meshes[0]
+    this._HandL = meshes[1]
+    this._LegL = meshes[2]
+    this._Head = meshes[3]
+    this._LegR = meshes[4]
+    this._HandR = meshes[5]
+
+    const animRotateL = new Animation("animRotateL", "rotation.x", 60, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_CYCLE),
+    animRotateR = new Animation("animRotateR", "rotation.x", 60, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_CYCLE)
+
+    animRotateL.setKeys([ { frame: 0,  value: 0 }, { frame: 15,  value: Math.PI / 4 }, { frame: 30,  value: 0 }, { frame: 45,  value: - Math.PI / 4 }, { frame: 60,  value: 0 } ])
+    animRotateR.setKeys([ { frame: 0,  value: 0 }, { frame: 15,  value: - Math.PI / 4 }, { frame: 30,  value: 0 }, { frame: 45,  value: Math.PI / 4 }, { frame: 60,  value: 0 } ])
+
+    this._HandL.animations.push(animRotateL)
+    this._LegL.animations.push(animRotateR)
+    this._HandR.animations.push(animRotateR)
+    this._LegR.animations.push(animRotateL)
+    
+    this._Head.setParent(this._Body)
+    this._HandL.setParent(this._Body)
+    this._HandR.setParent(this._Body)
+    this._LegL.setParent(this._Body)
+    this._LegR.setParent(this._Body)
+  }
+
+  public get model () {
+    return this._Body
+  }
+
+  public setColors(body: string, pants: string, skin: string) {
+    const bm = this._Body.material as MultiMaterial,
+      bmb = bm.subMaterials[0] as PBRMaterial,
+      bmp = bm.subMaterials[1] as PBRMaterial,
+      hlm = this._HandL.material as MultiMaterial,
+      hlmb = hlm.subMaterials[0] as PBRMaterial,
+      hrm = this._HandR.material as MultiMaterial,
+      hrmb = hrm.subMaterials[0] as PBRMaterial,
+      llmp = this._LegL.material as PBRMaterial,
+      lrmp = this._LegR.material as PBRMaterial
+    
+    bmb.albedoColor = Color3.FromHexString(body)
+    hlmb.albedoColor = Color3.FromHexString(body)
+    hrmb.albedoColor = Color3.FromHexString(body)
+
+    bmp.albedoColor = Color3.FromHexString(pants)
+    llmp.albedoColor = Color3.FromHexString(pants)
+    lrmp.albedoColor = Color3.FromHexString(pants)
+  }
+
+  public Idle() {
+    this._scene.stopAnimation(this._LegL)
+    this._scene.stopAnimation(this._LegR)
+    this._scene.stopAnimation(this._HandR)
+    this._scene.stopAnimation(this._HandL)
+    this._LegL.rotation.x = 0
+    this._LegR.rotation.x = 0
+    this._HandR.rotation.x = 0
+    this._HandL.rotation.x = 0
+  }
+
+  public Walk() {
+    this._scene.beginAnimation(this._LegL, 0, 60, true)
+    this._scene.beginAnimation(this._LegR, 0, 60, true)
+    this._scene.beginAnimation(this._HandR, 0, 60, true)
+    this._scene.beginAnimation(this._HandL, 0, 60, true)
+  }
+}
+
 export type {
   LegoMan
 }
-export default legoMan
+export default LegoManClass.Init
--- src/scenes/main.ts
+++ src/scenes/main.ts
@@ -1,6 +1,11 @@
 import { Engine, Scene, ArcRotateCamera, Vector3, Color3, SceneLoader, ShadowGenerator, DirectionalLight, HemisphericLight } from "@babylonjs/core"
 import legoMan, { LegoMan } from './components/legoman'
 
+
+const body_color = localStorage.getItem('body_color') || '#ff0000'
+const pants_color = localStorage.getItem('pants_color') || '#0000ff'
+const skin_color = localStorage.getItem('skin_color') || '#ffff00'
+
 const MainScene = (engine: Engine, canvas: HTMLCanvasElement) => {
   const scene = new Scene(engine)
   const camera = new ArcRotateCamera("camera1", Math.PI / 2, Math.PI / 4, 3, new Vector3(0, 0, 0), scene);
@@ -19,12 +24,13 @@ const MainScene = (engine: Engine, canvas: HTMLCanvasElement) => {
     var shadowGenerator = new ShadowGenerator(1024, light2);
     shadowGenerator.useBlurExponentialShadowMap = true;
     shadowGenerator.blurKernel = 32;
-    legoMan(scene).then((lm: LegoMan)=>{
-        lm.Walk()
-        setTimeout(()=>{
-            lm.Idle()
-        },2000)
-    })
+  legoMan(scene).then((lm: LegoMan)=>{
+    lm.setColors(body_color, pants_color, skin_color)
+    lm.Walk()
+    setTimeout(()=>{
+        lm.Idle()
+    },6000)
+  })
   return scene
 }
 export default MainScene
